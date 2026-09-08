fd27b404e4fd1675542993ae8ee261f60f40d961
Hong <1927225988@qq.com>
2026-09-08 20:38:33 +0800

    feat(pharmacy): 班次交班/支付明细/日统计服务

---
+++ backend/yudao-module-pharmacy/src/main/java/cn/iocoder/yudao/module/pharmacy/service/sale/impl/SalesStatisticsServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.iocoder.yudao.module.pharmacy.service.sale.impl;
+
+import cn.iocoder.yudao.module.pharmacy.dal.mysql.sale.SaleOrderMapper;
+import cn.iocoder.yudao.module.pharmacy.dal.mysql.sale.SaleOrderMapper.DailySaleRow;
+import cn.iocoder.yudao.module.pharmacy.service.sale.SalesStatisticsService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class SalesStatisticsServiceImpl implements SalesStatisticsService {
+
+    @Resource
+    private SaleOrderMapper saleOrderMapper;
+
+    @Override
+    public List<DailySaleRow> getDailyStatistics(Long storeId, LocalDateTime startTime, LocalDateTime endTime) {
+        return saleOrderMapper.selectDailySummary(storeId, startTime, endTime);
+    }
+}
